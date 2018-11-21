f122c41e7c51234f5ddd3864c5b76840d27f79ea
alex1504 <alex1504@163.com>
2018-11-21 09:47:18 +0800

    feat: update rename.py

--- rename.py
+++ rename.py
@@ -1,7 +1,17 @@
 import os
 
 def main():
+    cwd = os.getcwd()
+    imgDir = os.path.join(cwd, 'images')
     files = os.listdir('images')
+    count = 1
+    for file in files:
+        src = os.path.join(imgDir, file)
+        filename = os.path.splitext(file)[0]
+        ext = os.path.splitext(file)[1]
+        dist = os.path.join(imgDir, str(count) + ext)
+        os.rename(src, dist)
+        count += 1
 
 if __name__ == "__main__":
     main()
